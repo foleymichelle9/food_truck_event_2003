a37bcb9a40aba46b18b03f4b5737315f99706812
michelle <foleymichelle9@gmail.com>
2020-04-15 10:24:49 -0600

    Finish iteration 1

---
+++ test/food_truck_test.rb
@@ -0,0 +1,48 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/item'
+require './lib/food_truck'
+
+class Test < Minitest::Test
+
+  def setup
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+  end
+
+  def test_it_exists
+    assert_instance_of FoodTruck, @food_truck
+  end
+
+  def test_it_has_a_name
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+  end
+
+  def test_inventory_is_empty
+    assert_equal ({}), @food_truck.inventory
+  end
+
+  def test_item_is_zero_by_default
+    assert_equal 0, @food_truck.check_stock(@item1)
+  end
+
+  def test_it_can_stock_items
+    @food_truck.stock(@item1, 30)
+    assert_equal ({@item1=>30}), @food_truck.inventory
+  end
+
+  def test_check_stock_returns_amount
+    @food_truck.stock(@item1, 30)
+    assert_equal 30, @food_truck.check_stock(@item1)
+    @food_truck.stock(@item1, 25)
+    assert_equal 55, @food_truck.check_stock(@item1)
+  end
+
+  def test_it_can_update_inventory
+    @food_truck.stock(@item1, 30)
+    @food_truck.stock(@item1, 25)
+    @food_truck.stock(@item2, 12)
+    assert_equal ({@item1=>55, @item2=>12}), @food_truck.inventory
+  end
+end
